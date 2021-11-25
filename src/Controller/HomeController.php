61c9cac7a0e82e49c9138fa21a85383c3c52b3a7
Sayn <marvincrepin@gmail.com>
2021-11-25 16:53:27 +0100

    Form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,18 +21,21 @@ class HomeController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index()
-    {
-        return $this->twig->render('Home/index.html.twig');
-    }
     public function search()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+    }
+
+    public function index()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $instrument = $_POST['instrument'];
             $address = $_POST['address'];
             $userManager = new UserManager();
             $users = $userManager->selectAllBySearch($instrument, $address);
             return $this->twig->render('Home/search.html.twig', ['users' =>  $users]);
         }
+
+        return $this->twig->render('Home/index.html.twig');
     }
 }
